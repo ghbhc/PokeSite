c124dafc27394aa2bd6f75bb266f7b16407f5cc6
Ben Cronheim <bc06026@uga.edu>
2022-03-18 16:55:38 -0400

    Clean up and better organize web-scraping script
    Script checks whether or not database already has 151
    Pokemon objects in it before starting to scrape info for
    each Pokemon.

--- get-poke-data.py
+++ get-poke-data.py
@@ -12,10 +12,9 @@ usage (from project dir):
 ./manage.py shell < get-poke-data.py
 '''
 
-
-poke_list = ['Bulbasaur', 'Ivysaur', 'Venusaur', 'Charmander', 'Charmeleon', 'Charizard', 'Squirtle', 'Wartortle', 'Blastoise', 'Caterpie', 'Metapod', 'Butterfree', 'Weedle', 'Kakuna', 'Beedrill', 'Pidgey', 'Pidgeotto', 'Pidgeot', 'Rattata', 'Raticate', 'Spearow', 'Fearow', 'Ekans', 'Arbok', 'Pikachu', 'Raichu', 'Sandshrew', 'Sandslash', 'nidoran-female', 'Nidorina', 'Nidoqueen', 'nidoran-male', 'Nidorino', 'Nidoking', 'Clefairy', 'Clefable', 'Vulpix', 'Ninetales', 'Jigglypuff', 'Wigglytuff', 'Zubat', 'Golbat', 'Oddish', 'Gloom', 'Vileplume', 'Paras', 'Parasect', 'Venonat', 'Venomoth', 'Diglett', 'Dugtrio', 'Meowth', 'Persian', 'Psyduck', 'Golduck', 'Mankey', 'Primeape', 'Growlithe', 'Arcanine', 'Poliwag', 'Poliwhirl', 'Poliwrath', 'Abra', 'Kadabra', 'Alakazam', 'Machop', 'Machoke', 'Machamp', 'Bellsprout', 'Weepinbell', 'Victreebel', 'Tentacool', 'Tentacruel', 'Geodude', 'Graveler', 'Golem', 'Ponyta', 'Rapidash', 'Slowpoke', 'Slowbro', 'Magnemite', 'Magneton', 'farfetchd', 'Doduo', 'Dodrio', 'Seel', 'Dewgong', 'Grimer', 'Muk', 'Shellder', 'Cloyster', 'Gastly', 'Haunter', 'Gengar', 'Onix', 'Drowzee', 'Hypno', 'Krabby', 'Kingler', 'Voltorb', 'Electrode', 'Exeggcute', 'Exeggutor', 'Cubone', 'Marowak', 'Hitmonlee', 'Hitmonchan', 'Lickitung', 'Koffing', 'Weezing', 'Rhyhorn', 'Rhydon', 'Chansey', 'Tangela', 'Kangaskhan', 'Horsea', 'Seadra', 'Goldeen', 'Seaking', 'Staryu', 'Starmie', 'mr-mime', 'Scyther', 'Jynx', 'Electabuzz', 'Magmar', 'Pinsir', 'Tauros', 'Magikarp', 'Gyarados', 'Lapras', 'Ditto', 'Eevee', 'Vaporeon', 'Jolteon', 'Flareon', 'Porygon', 'Omanyte', 'Omastar', 'Kabuto', 'Kabutops', 'Aerodactyl', 'Snorlax', 'Articuno', 'Zapdos', 'Moltres', 'Dratini', 'Dragonair', 'Dragonite', 'Mewtwo', 'Mew']
-
-base_url = "https://pokemon.com/us/pokedex/"
+####################
+# Get Pokemon Images
+####################
 
 # make sure static/images dir exists
 img_dir = Path('static/images')
@@ -24,41 +23,68 @@ if not img_dir.exists():
 
 # move on if images alredy has 151 things in it
 if len(os.listdir('static/images')) == 151:
-    pass
+    print('static/images already has images for all 151 Pokemon')
 else:
     # get images for each pokemon
     images_base_url = 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/'
-    print('getting pokemon images...')
+    print('getting Pokemon images...')
     for n in range(1,152):
         pic = requests.get(f'{images_base_url}/{n:03}.png')
         with open(f'{img_dir}/{n:03}.png', 'wb') as f:
             f.write(pic.content)
 
-for index, pokemon in enumerate(poke_list):
+##################
+# Get Pokemon Info
+##################
 
-    print(f'Processing {pokemon}...')
+# check if database already has all 151 Pokemon in it
+if len(Pokemon.objects.all()) == 151:
+    print('Database already has all 151 Pokemon')
+else:
+    poke_list = [
+        'Bulbasaur', 'Ivysaur', 'Venusaur', 'Charmander', 'Charmeleon', 'Charizard', 'Squirtle', 'Wartortle', 'Blastoise',
+        'Caterpie', 'Metapod', 'Butterfree', 'Weedle', 'Kakuna', 'Beedrill', 'Pidgey', 'Pidgeotto', 'Pidgeot', 'Rattata', 
+        'Raticate', 'Spearow', 'Fearow', 'Ekans', 'Arbok', 'Pikachu', 'Raichu', 'Sandshrew', 'Sandslash', 'nidoran-female', 
+        'Nidorina', 'Nidoqueen', 'nidoran-male', 'Nidorino', 'Nidoking', 'Clefairy', 'Clefable', 'Vulpix', 'Ninetales', 
+        'Jigglypuff', 'Wigglytuff', 'Zubat', 'Golbat', 'Oddish', 'Gloom', 'Vileplume', 'Paras', 'Parasect', 'Venonat', 
+        'Venomoth', 'Diglett', 'Dugtrio', 'Meowth', 'Persian', 'Psyduck', 'Golduck', 'Mankey', 'Primeape', 'Growlithe', 
+        'Arcanine', 'Poliwag', 'Poliwhirl', 'Poliwrath', 'Abra', 'Kadabra', 'Alakazam', 'Machop', 'Machoke', 'Machamp', 
+        'Bellsprout', 'Weepinbell', 'Victreebel', 'Tentacool', 'Tentacruel', 'Geodude', 'Graveler', 'Golem', 'Ponyta', 
+        'Rapidash', 'Slowpoke', 'Slowbro', 'Magnemite', 'Magneton', 'farfetchd', 'Doduo', 'Dodrio', 'Seel', 'Dewgong', 
+        'Grimer', 'Muk', 'Shellder', 'Cloyster', 'Gastly', 'Haunter', 'Gengar', 'Onix', 'Drowzee', 'Hypno', 'Krabby', 
+        'Kingler', 'Voltorb', 'Electrode', 'Exeggcute', 'Exeggutor', 'Cubone', 'Marowak', 'Hitmonlee', 'Hitmonchan', 
+        'Lickitung', 'Koffing', 'Weezing', 'Rhyhorn', 'Rhydon', 'Chansey', 'Tangela', 'Kangaskhan', 'Horsea', 'Seadra', 
+        'Goldeen', 'Seaking', 'Staryu', 'Starmie', 'mr-mime', 'Scyther', 'Jynx', 'Electabuzz', 'Magmar', 'Pinsir', 
+        'Tauros', 'Magikarp', 'Gyarados', 'Lapras', 'Ditto', 'Eevee', 'Vaporeon', 'Jolteon', 'Flareon', 'Porygon', 
+        'Omanyte', 'Omastar', 'Kabuto', 'Kabutops', 'Aerodactyl', 'Snorlax', 'Articuno', 'Zapdos', 'Moltres', 'Dratini', 
+        'Dragonair', 'Dragonite', 'Mewtwo', 'Mew'
+    ]
 
-    # get web page for Pokemon
-    r = requests.get(f"{base_url}/{pokemon}")
-    # parse with BeautifulSoup
-    soup = BeautifulSoup(r.content, "html.parser")
+    # get types, height, weight, and description for each pokemon
+    base_url = "https://pokemon.com/us/pokedex/"
+    print('getting Pokemon info...')
+    for index, pokemon in enumerate(poke_list):
+        # get web page for Pokemon
+        r = requests.get(f"{base_url}/{pokemon}")
+        # parse with BeautifulSoup
+        soup = BeautifulSoup(r.content, "html.parser")
 
-    # get Pokemon type(s)
-    type_info =  soup.find("div", class_="dtm-type").find_all('a')
-    type1 = type_info[0].text
-    if len(type_info) == 2:
-        type2 = type_info[1].text
-    else:
-        type2 = None 
+        # get Pokemon type(s)
+        type_info =  soup.find("div", class_="dtm-type").find_all('a')
+        type1 = type_info[0].text
+        if len(type_info) == 2:
+            type2 = type_info[1].text
+        else:
+            type2 = None 
 
-    # get Pokemon height and weight
-    height = soup.find_all("span", class_="attribute-value")[0].text
-    weight = soup.find_all("span", class_="attribute-value")[1].text
+        # get Pokemon height and weight
+        height = soup.find_all("span", class_="attribute-value")[0].text
+        weight = soup.find_all("span", class_="attribute-value")[1].text
 
-    # get Pokemon description
-    description = soup.find("p", class_="version-x").text.strip()
+        # get Pokemon description
+        description = soup.find("p", class_="version-x").text.strip()
 
-    # put Pokemon data in database
-    pokemon_obj = Pokemon(number=index + 1, name=pokemon, type1=type1, type2=type2, height=height, weight=weight, description=description)
-    pokemon_obj.save()
+        # put Pokemon data in database
+        pokemon_obj = Pokemon(number=index + 1, name=pokemon, type1=type1, type2=type2, height=height, weight=weight, description=description)
+        pokemon_obj.save()
 
