46c11e864f88f7e7a975aee05b79b1ce941ed26e
Ben Cronheim <bc06026@uga.edu>
2022-03-18 16:14:05 -0400

    Add initial Pokemon model

--- dex/models.py
+++ dex/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+class Pokemon(models.Model):
+    number = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=32)
+    type1 = models.CharField(max_length=16)
+    type2 = models.CharField(max_length=16,null=True)
+    height = models.CharField(max_length=8)
+    weight = models.CharField(max_length=8)
+    description = models.CharField(max_length=256)
+
+    class Meta:
+        verbose_name_plural = 'Pokemon'
+        ordering = ['number',]
+    
+    def __str__(self):
+        return self.name
