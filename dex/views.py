0470db3c344d5aa69e32624a6dd50a1eb46fc4a1
Ben Cronheim <bc06026@uga.edu>
2022-03-21 16:50:58 -0400

    Add initial index page
    Add urls, view, and css for initial index page

--- dex/views.py
+++ dex/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.db import models
+from .models import Pokemon
 
 # Create your views here.
+
+def index(request):
+    num_pokemon = Pokemon.objects.all().count()
+    type_count = Pokemon.objects.values('type1').annotate(n=models.Count('type1')).order_by('-n')
+
+    context = {
+        'num_pokemon': num_pokemon,
+        'type_count': type_count,
+    }
+    
+    return render(request, 'dex/index.html', context=context)
