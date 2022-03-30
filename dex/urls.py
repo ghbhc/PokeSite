d41a1d55e86e619b68c600be4612065313899ef0
Ben Cronheim <bc06026@uga.edu>
2022-03-29 20:51:27 -0400

    Add initial view, template, and url for primary Pokemon types

--- dex/urls.py
+++ dex/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('type/<str:_type>', views.typeListView, name='type_list'),
 ]
--- dex/views.py
+++ dex/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
 from django.db import models
+from django.views import generic
 from .models import Pokemon
 
-# Create your views here.
-
 def index(request):
+    '''
+    home page 
+    '''
     num_pokemon = Pokemon.objects.all().count()
     type_count = Pokemon.objects.values('type1').annotate(n=models.Count('type1')).order_by('-n')
 
@@ -14,3 +16,14 @@ def index(request):
     }
     
     return render(request, 'dex/index.html', context=context)
+
+def typeListView(request, _type):
+    '''
+    view to display Pokemon having a specific primary type
+    '''
+    type_list = Pokemon.objects.filter(type1=_type.capitalize())
+    context = {
+        'type_list': type_list,
+    }
+    
+    return render(request, 'dex/type.html', context=context)
