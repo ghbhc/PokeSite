6e87db8970bfb0c3ab08b3260ddc401b3828bf67
Ben Cronheim <bc06026@uga.edu>
2022-04-03 17:24:54 -0400

    Sort drop down menu alphabetically

--- dex/context_processors.py
+++ dex/context_processors.py
@@ -1,8 +1,9 @@
+from django.db import models
 from .models import Pokemon
 
 def navTypes(request):
 
-    types = Pokemon.objects.order_by().values('type1').distinct()
+    types = Pokemon.objects.order_by('type1').values('type1').distinct()
     return {
         'types': types
     }
